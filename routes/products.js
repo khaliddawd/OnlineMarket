e29359493e97c7e126508ed4de646cb47124e82e
Khalid <khalid@Khalids-MacBook-Pro.local>
2023-10-21 17:05:32 -0400

    Your commit message here

--- routes/products.js
+++ routes/products.js
@@ -1,16 +1,30 @@
 // routes/products.js
+
 const express = require('express');
 const Product = require('../models/product');
 
 const router = express.Router();
 
-// GET all
+// GET all products
 router.get('/', async (req, res) => {
   try {
     const products = await Product.find();
     res.json(products);
   } catch (err) {
     res.status(500).json({ message: err.message });
+  }
+});
+
+// GET a single product by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product == null) {
+      return res.status(404).json({ message: 'Cannot find product' });
+    }
+    res.json(product);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
   }
 });
 
@@ -32,6 +46,48 @@ router.post('/', async (req, res) => {
   }
 });
 
-// ... other routes (PUT, DELETE, etc.)
+// PUT request to update a product by ID
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, updates, { new: true });
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.json(updatedProduct);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// DELETE request to delete a product by ID
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.json({ message: 'Product deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+
+});
+router.delete('/', async (req, res) => {
+  try {
+      await Product.deleteMany({});
+      res.json({ message: 'All products deleted' });
+  } catch (err) {
+      res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
